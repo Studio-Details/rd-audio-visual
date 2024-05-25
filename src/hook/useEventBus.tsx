5026b274fb28971a609d88a97fd7d45c793b1c43
Shinya Abe <s-abe@details.co.jp>
2024-05-25 23:15:51 +0900

    add evntBus useEventBus

---
+++ src/hook/useEventBus.tsx
@@ -0,0 +1,10 @@
+import eventBus from '@utils/eventBus'
+
+// EventBusの機能を提供するhook
+
+const _eventBus = eventBus<Record<string, any>>()
+const useEventBus = () => {
+  return _eventBus
+}
+
+export default useEventBus
